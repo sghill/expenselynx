f989fcc3fc17d3cb485c04d2b9978c1e6860263c
sghill <devnull@localhost>
2010-10-24 16:50:36 -0400

    store scaffolded with name

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Expenselynx::Application.routes.draw do
+  resources :stores
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ db/migrate/20101024205015_create_stores.rb
@@ -0,0 +1,13 @@
+class CreateStores < ActiveRecord::Migration
+  def self.up
+    create_table :stores do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :stores
+  end
+end
