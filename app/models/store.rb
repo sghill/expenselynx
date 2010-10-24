3566e7bb8d153f84da7b384e6c88a870147c0228
sghill <devnull@localhost>
2010-10-24 17:25:52 -0400

    store requires a name to be created

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,3 @@
 class Store < ActiveRecord::Base
+  validates :name, :presence => true
 end
--- test/unit/store_test.rb
+++ test/unit/store_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class StoreTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should save valid store" do
+    store = Store.new(:name => 'anything unique')
+    assert store.valid?
+  end
+  
+  test "should not save store without name" do
+    store = Store.new
+    assert store.invalid?
   end
 end
