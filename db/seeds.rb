01b31f77fa969353ee698de6e3f11df399629cbb
sghill <devnull@localhost>
2010-10-24 20:52:31 -0400

    receipt seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-stores = Store.create([ { :name => 'Target'}, 
-                        { :name => 'Microcenter'},
-                        { :name => 'Jewel-Osco'},
-                        { :name => 'The Gap'}])
+stores = Store.create([ { :name => 'Target' }, 
+                        { :name => 'Microcenter' },
+                        { :name => 'Jewel-Osco' },
+                        { :name => 'The Gap' }])
+
+receipts = Receipt.create([{ :store_id => stores.first.id, 
+                             :purchase_date => DateTime.now, 
+                             :total => 3.74 },
+                           { :store_id => stores.first.id,
+                             :purchase_date => DateTime.now - 3.days,
+                             :total => 21.37 },
+                           { :store_id => stores.last.id,
+                             :purchase_date => DateTime.now - 2.days,
+                             :total => 13.31 }])
