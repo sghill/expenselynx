b78dc4949f97c87fc9ca47f5633ef652447b8794
sghill <devnull@localhost>
2010-11-14 22:03:38 -0600

    expense report always must have a user

--- app/models/expense_report.rb
+++ app/models/expense_report.rb
@@ -1,3 +1,6 @@
 class ExpenseReport < ActiveRecord::Base
   has_many :receipts
+  belongs_to :user
+  
+  validates :user, :presence => true
 end
--- test/functional/expense_report_controller_test.rb
+++ test/functional/expense_report_controller_test.rb
@@ -1,9 +1,21 @@
 require 'test_helper'
 
 class ExpenseReportControllerTest < ActionController::TestCase
+  setup do
+    @sara = Factory(:sara)
+    @store = Factory(:chipotle)
+    @today = DateTime.now.to_date
+  end
+  
   test "should GET show" do
     get :show
     assert_response :success
   end
+  
+  # test "show should have a collection of expensable receipts" do
+  #     report
+  #     sign_in @sara
+  #     Receipt.create(:total => 1, :store => @store, :purchase_date => @today, :expensable => true, :expense_report => report)
+  #   end
 
 end
