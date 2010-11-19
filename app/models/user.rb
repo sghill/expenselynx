b7daee08fdba8cadf15afdcf99d110095609e05e
sghill <devnull@localhost>
2010-11-19 01:44:19 -0800

    user has many participants

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :expense_reports
-  has_many :receipts  
+  has_many :receipts
+  has_many :participants
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
