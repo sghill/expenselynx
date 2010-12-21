3bc66f1cda6964874a835b1269f837bc2603d60e
sghill <sghill.dev@gmail.com>
2010-12-20 22:04:45 -0800

    getting first expense category for csv export

--- app/controllers/expense_report_controller.rb
+++ app/controllers/expense_report_controller.rb
@@ -53,7 +53,7 @@ class ExpenseReportController < ApplicationController
       @csv_string = CSV.generate do |csv|
         @receipts.each do |receipt|
           store = Store.find_by_name(receipt.store.name)
-          expense_category = ExpenseCategory.find_by_name(store.expense_category.name) unless store.expense_category.nil?
+          expense_category = ExpenseCategory.find_by_name(store.expense_categories.first.name) unless store.expense_categories.empty?
           participant_names = receipt.participants.collect{ |p| p.name }
           participant_names << "me"
           csv << [expense_category.nil? ? "" : expense_category.name,
