92d5cd0b2e2e646ffd5dd313391114bd9556a332
Andrew Kiellor <akiellor@gmail.com>
2011-02-19 12:58:05 +1100

    Exposed the first API resource /api/user with Basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Expenselynx::Application.routes.draw do
+  namespace :api do
+    match 'user' => 'users#current', :defaults => { :format => 'json' }
+  end
+
   match "participants/search" => 'participant#search'
   match "participants" => 'participant#index'
   match "participants/merge" => 'participant#merge'
   match "participants/merge_zone" => 'participant#merge_zone'
-  
+
   match "expense_report/:id/download" => "expense_report#download_csv"
-  
+
   get "expense_report/:id" => "expense_report#show", :as => "expense_report"
   post "expense_report/create"
   devise_for :users
