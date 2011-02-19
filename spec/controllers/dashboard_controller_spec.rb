c430ef31ac6e21a01721c26d40b78f16d4cb801c
Andrew Kiellor <akiellor@gmail.com>
2011-02-19 21:10:20 +1100

    Migrated all of the dashboard TestUnit tests to RSpec

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -5,43 +5,50 @@ describe DashboardController do
   include RSpecMacros
   include ControllerMacros
 
-  let!(:sara) { Factory(:sara) }
-
-  context "sara has 5 receipts" do
-    let(:saras_receipts) { [mock_model(Receipt),
-                            mock_model(Receipt),
-                            mock_model(Receipt)] }
-
-    let(:saras_expense_reports) { [mock_model(ExpenseReport),
-                                   mock_model(ExpenseReport)] }
-
-    before :each do
-      sign_in sara
-      sara.stub(:receipts).and_return(saras_receipts)
-      sara.stub_chain(:expense_reports, :recent).and_return(saras_expense_reports)
-      controller.stub(:current_user).and_return(sara)
-      Timecop.freeze
-    end
-
-    describe_action :index do
-      the_assigned(:receipts) { should =~ saras_receipts }
-      the_assigned(:receipt) { should be_a Receipt }
-      the_assigned(:receipt, :purchase_date) { should == Time.now.to_date }
-      the_assigned(:reports) { should == saras_expense_reports }
+  context "no-one is signed in" do
+    describe_actions :index, :unexpensed, :projects do
+      the(:response) { should redirect_to(new_user_session_path) }
     end
   end
 
-  context "sara has 4 projects" do
-    let(:saras_projects) { [mock_model(Project),
-                            mock_model(Project),
-                            mock_model(Project),
-                            mock_model(Project)]}
+  context "sara is signed in" do
+    let(:sara) { Factory(:sara) }
 
-    context "sara is signed in" do
+    before :each do
+      sign_in sara
+      controller.stub(:current_user).and_return(sara)
+    end
+
+    context "sara has 5 receipts" do
+      let(:saras_receipts) { [mock_model(Receipt),
+                              mock_model(Receipt),
+                              mock_model(Receipt)] }
+
+      let(:saras_expense_reports) { [mock_model(ExpenseReport),
+                                     mock_model(ExpenseReport)] }
+
       before :each do
-        sign_in sara
+        sara.stub(:receipts).and_return(saras_receipts)
+        sara.stub_chain(:expense_reports, :recent).and_return(saras_expense_reports)
+        Timecop.freeze
+      end
+
+      describe_action :index do
+        the_assigned(:receipts) { should =~ saras_receipts }
+        the_assigned(:receipt) { should be_a Receipt }
+        the_assigned(:receipt, :purchase_date) { should == Time.now.to_date }
+        the_assigned(:reports) { should == saras_expense_reports }
+      end
+    end
+
+    context "sara has 4 projects" do
+      let(:saras_projects) { [mock_model(Project),
+                              mock_model(Project),
+                              mock_model(Project),
+                              mock_model(Project)] }
+
+      before :each do
         sara.stub(:projects).and_return(saras_projects)
-        controller.stub(:current_user).and_return(sara)
       end
 
       describe_action :projects do
@@ -49,34 +56,18 @@ describe DashboardController do
       end
     end
 
-    context "no-one is signed in" do
-      describe_action :projects do
-        the(:response) { should redirect_to(new_user_session_path) }
-      end
-    end
-  end
+    context "sara has 4 unexpensed receipts" do
+      let(:saras_unexpensed_receipts) { [mock_model(Receipt),
+                                         mock_model(Receipt),
+                                         mock_model(Receipt),
+                                         mock_model(Receipt)] }
 
-  context "sara has 4 unexpensed receipts" do
-    let(:saras_unexpensed_receipts) { [mock_model(Receipt),
-                                       mock_model(Receipt),
-                                       mock_model(Receipt),
-                                       mock_model(Receipt)]}
-
-    context "sara is signed in" do
       before :each do
-        sign_in sara
         sara.stub_chain(:receipts, :unexpensed).and_return(saras_unexpensed_receipts)
-        controller.stub(:current_user).and_return(sara)
       end
 
       describe_action :unexpensed do
         the_assigned(:receipts) { should == saras_unexpensed_receipts }
-      end
-    end
-
-    context "no-one is signed in" do
-      describe_action :unexpensed do
-        the(:response) { should redirect_to(new_user_session_path) }
       end
     end
   end
