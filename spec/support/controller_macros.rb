b3eb8005db410d156b88dd5e0ae3719eb999db01
Andrew Kiellor <akiellor@gmail.com>
2011-02-19 21:10:20 +1100

    Migrated all of the dashboard TestUnit tests to RSpec

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -14,6 +14,12 @@ module ControllerMacros
       end
     end
 
+    def describe_actions *action_names, &block
+      action_names.each do |action_name|
+        describe_action action_name, &block
+      end
+    end
+
     def the_assigned assign_name, *attrs, &block
       describe "the assigned #{assign_name} #{attrs.join(' ')}" do
         subject { attrs.inject(assigns(assign_name)) {|result, attr| result.send(attr) } }
