14cf5fdbc540870c0fa11c9d6b9a19a7ac7a0b74
Andrew Kiellor <akiellor@gmail.com>
2011-02-21 01:57:32 +1100

    Added create receipt to api

---
+++ app/controllers/api/receipts_controller.rb
@@ -0,0 +1,29 @@
+class Api::ReceiptsController < ApplicationController
+  before_filter :authenticate_user!
+
+  respond_to :json, :xml
+
+  expose(:receipts) do
+    current_user.receipts
+  end
+
+  expose(:receipt) do
+    current_user.receipts.find(params[:id])
+  end
+
+  expose(:form) do
+    Api::Forms::CreateReceiptForm.new
+  end
+
+  def index
+    respond_with receipts
+  end
+
+  def show
+    respond_with receipt
+  end
+
+  def create
+    form.fill_in(request, current_user).receipt.save!
+  end
+end
