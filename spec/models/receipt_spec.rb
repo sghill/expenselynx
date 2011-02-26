1fb2375edc59d6b30070d25e3f5f6d35aa1bc9d1
Andrew Kiellor <akiellor@gmail.com>
2011-02-26 12:52:21 +1100

    Migrated store_test to RSpec

--- spec/models/receipt_spec.rb
+++ spec/models/receipt_spec.rb
@@ -195,19 +195,3 @@ describe Receipt do
     end
   end
 end
-
-RSpec::Matchers.define :have_error_message do |message|
-  @on = :base
-  chain(:on) {|on| @on = on}
-  match do |model|
-    errors = model.errors[@on] and errors.include? message
-  end
-
-  failure_message_for_should do |model|
-"""
-expected model to have error message '#{message}' on '#{@on}' but didn't. Had:
-#{model.errors[@on].inspect}
-
-"""
-  end
-end
--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -1,18 +1,67 @@
 require 'spec_helper'
 
 describe Store do
-  it "should have many expense categories" do
-    store = Store.new(:name => 'Target')
-    store.expense_categories.should be_an_instance_of(Array)
+  let(:name) { nil }
+
+  subject do
+    store = Store.new(:name => name)
+    store.valid?
+    store
   end
-  
-  it "should have many user store expense categories" do
-    store = Store.new(:name => 'Meijer')
-    store.user_store_expense_categories.should be_an_instance_of(Array)
+
+  context "with name 'Target'" do
+    let(:name) { 'Target' }
+    its(:expense_categories) { should be_an_instance_of(Array) }
+    its(:user_store_expense_categories) { should be_an_instance_of(Array) }
+    its(:users) { should be_an_instance_of(Array) }
+    it { should be_valid }
   end
-  
-  it "should have many users" do
-    store = Store.new(:name => 'Starbox')
-    store.users.should be_an_instance_of(Array)
+
+  context "without a name" do
+    let(:name) { nil }
+
+    it { should be_invalid }
+    it { should have_error_message("can't be blank").on(:name) }
+  end
+
+  context "with a name the same as an existing store" do
+    let(:name) { 'Old Navy' }
+    before { Store.create(:name => name) }
+
+    it { should be_invalid }
+    it { should have_error_message("has already been taken").on(:name) }
+  end
+
+  context "with a name the same but different case as an existing store" do
+    let(:name) { 'la shish' }
+
+    before { Store.create(:name => 'La Shish') }
+
+    it { should be_invalid }
+    it { should have_error_message("has already been taken").on(:name) }
+  end
+
+  context "with name 'Typical Party Store'" do
+    let(:name) { 'Typical Party Store' }
+
+    before { subject.save! }
+
+    it { should == Store.find_by_name("typical party stoRE") }
+    it { should == Store.find_by_name(:case_sensitive => true, :name => "Typical Party Store") }
+    it { Store.find_all_by_name('typical party stoRE').count.should == 1 }
+
+    it "should not create store when one exists case insensitive" do
+      Store.find_or_create_by_name(:case_sensitive => false, :name => 'typical party stoRE')
+      Store.find_all_by_name('Typical Party Store').count.should == 1
+    end
+
+    it "should not create when one with same name exists" do
+      should == Store.find_or_create_by_name('Typical Party Store')
+    end
+
+    it "should search and find store by partial name case insensitive" do
+      found_store = Store.search_by_name("typ").first
+      should == found_store
+    end
   end
 end
