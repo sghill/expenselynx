0574c2841f70ac0fed1956debed27174e0925201
SG Hill <sg@sghill.net>
2011-03-19 03:03:02 -0400

    reordered Participant Specs for better readability in rspec doc mode

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -5,39 +5,45 @@ describe Participant do
     @sara = Factory(:sara)
   end
   
-  it "should require a name" do
-    participant = Participant.new(:user => @sara)
-    participant.should_not be_valid
+  describe :properties do
+    it "should have a name of type string" do
+      participant = Participant.new(:name => "turtle", :user => @sara)
+      participant.name.should be_a_kind_of(String)
+    end
   end
   
-  it "should have a name of type string" do
-    participant = Participant.new(:name => "turtle", :user => @sara)
-    participant.name.should be_a_kind_of(String)
+  describe :relationships do
+    it "should belong to many receipts" do
+      participant = Participant.new(:name => "frank", :user => @sara)
+      participant.receipts.should be_an_instance_of(Array)
+    end
+
+    it "must belong to a user" do
+      participant = Participant.new(:name => "alf")
+      participant.should_not be_valid
+    end
   end
   
-  it "should belong to many receipts" do
-    participant = Participant.new(:name => "frank", :user => @sara)
-    participant.receipts.should be_an_instance_of(Array)
+  describe :validations do
+    it "should require a name" do
+      participant = Participant.new(:user => @sara)
+      participant.should_not be_valid
+    end
+    
+    it "should not be saved with the same user and name" do
+      Participant.create(:name => "frank", :user => @sara)
+      participant2 = Participant.new(:name => "frank", :user => @sara)
+      participant2.should_not be_valid
+    end
+  
+    it "should not be saved with the same user and name case insensitive" do
+      Participant.create(:name => "frank", :user => @sara)
+      participant2 = Participant.new(:name => "fRaNk", :user => @sara)
+      participant2.should_not be_valid
+    end
   end
   
-  it "must belong to a user" do
-    participant = Participant.new(:name => "alf")
-    participant.should_not be_valid
-  end
-  
-  it "should not be saved with the same user and name" do
-    Participant.create(:name => "frank", :user => @sara)
-    participant2 = Participant.new(:name => "frank", :user => @sara)
-    participant2.should_not be_valid
-  end
-  
-  it "should not be saved with the same user and name case insensitive" do
-    Participant.create(:name => "frank", :user => @sara)
-    participant2 = Participant.new(:name => "fRaNk", :user => @sara)
-    participant2.should_not be_valid
-  end
-  
-  context "==" do
+  describe :== do
     it "should be == to a duplicate object with the same name" do
       testarossa = Participant.new(:name => "testarossa")
       dup_testa = Participant.new(:name => "testarossa")
@@ -45,7 +51,7 @@ describe Participant do
     end
   end
   
-  context "searching" do
+  describe :searching do
     it "should find or create by regardless of case" do
       original_tom = Participant.create(:name => "tom", :user => @sara)
       participant2 = Participant.find_or_create_by_name("TOM")
