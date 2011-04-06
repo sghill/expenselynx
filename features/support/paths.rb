e6550efb4c2f342f0c35d12000a443bc7e244e59
SG Hill <sg@sghill.net>
2011-04-05 19:56:13 -0700

    cucumber-rails updated, rails 3.0.6, build _should_ be passing

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -8,35 +8,30 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-      when /the home\s?page/
-        '/'
+    when /the home\s?page/
+      '/'
+    when /the registration page/
+      new_user_registration_path
+    when /the login page/
+      new_user_session_path
+    when /the dashboard page/
+      root_path
+    when /the unexpensed receipts page/
+      dashboard_unexpensed_path
+    when /the expense report page/
+      expense_report_path(ExpenseReport.last)
+    when /the edit participant page for "(.*)"/
+      edit_participant_path(Participant.find_by_name($1))
 
-      # Add more mappings here.
-      # Here is an example that pulls values out of the Regexp:
-      #
-      #   when /^(.*)'s profile page$/i
-      #     user_profile_path(User.find_by_login($1))
-      when /the registration page/
-        new_user_registration_path
-      when /the login page/
-        new_user_session_path
-      when /the dashboard page/
-        root_path
-      when /the unexpensed receipts page/
-        dashboard_unexpensed_path
-      when /the expense report page/
-        expense_report_path(ExpenseReport.last)
-      when /the edit participant page for "(.*)"/
-        edit_participant_path(Participant.find_by_name($1))
-      else
-        begin
-          page_name =~ /the (.*) page/
-          path_components = $1.split(/\s+/)
-          self.send(path_components.push('path').join('_').to_sym)
-        rescue Object => e
-          raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
-                  "Now, go and add a mapping in #{__FILE__}"
-        end
+    else
+      begin
+        page_name =~ /the (.*) page/
+        path_components = $1.split(/\s+/)
+        self.send(path_components.push('path').join('_').to_sym)
+      rescue Object => e
+        raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+          "Now, go and add a mapping in #{__FILE__}"
+      end
     end
   end
 end
