8ca08ffddc19c999a3b91bb0b8a52aa07a7623bc
sghill <sg@sghill.net>
2011-05-25 23:06:45 -0700

    refactored stores controller tests

--- spec/controllers/stores_controller_spec.rb
+++ spec/controllers/stores_controller_spec.rb
@@ -2,99 +2,109 @@ require 'spec_helper'
 
 describe StoresController do
   include Devise::TestHelpers
-
-  context "with users sara and john and store 'Ace Hardware'" do
-    before do
-      @sara = Factory(:sara)
-      @john = Factory(:user)
-      @store = Store.create(:name => "Ace Hardware")
-    end
-
-    context "GET edit" do
-      it "should require user to log in to see a store for editing" do
-        get :edit, :id => @store.to_param
-        response.should redirect_to(new_user_session_path)
-      end
-
-      it "should contain the specified store" do
-        sign_in @sara
-        get :edit, :id => @store.to_param
-        assigns(:store).name.should == @store.name
-      end
-    end
-
-    context "PUT update" do
-      it "should not change the store name for a normal user" do
-        sign_in @sara
-        put :update, :id => @store.to_param, :store => Store.new(:name => 'Coffee Place')
-        assigns(:store).name.should_not == "Coffee Place"
-      end
-
-      it "should allow the user to assign an expense category" do
-        sign_in @sara
-        put :update, :id => @store.id, :store => {:name => 'Abc', :expense_categories => "airfare"}
-        assigns(:store).expense_categories.should_not be_nil
-      end
-
-      it "should have different expense categories per store per user" do
-        sign_in @sara
-        put :update, :id => @store.id, :store => {:name => 'Abc', :expense_categories => "airfare"}
-        sign_out @sara
-        sign_in @john
-        put :update, :id => @store.id, :store => {:name => 'Abc', :expense_categories => "business meals"}
-
-        get :show, :id => @store.id
-        assigns(:expense_categories).length.should == 1
-        assigns(:expense_categories).first.name.should == "business meals"
-      end
-    end
-
-    context "GET show" do
-      it "should require login" do
-        get :show, :id => @store.id
-        response.should redirect_to(new_user_session_path)
-      end
-
-      context "sara is logged in" do
-        before :each do
-          sign_in @sara
-        end
-        it "should contain the specified store" do
-          get :show, :id => @store.id
-          assigns(:store).name.should == @store.name
-        end
-
-        it "should show the expense categories for current user and store" do
-          ExpenseCategory.should_receive(:for_user_and_store).with(@sara.id, @store.id)
-          get :show, :id => @store.id
-        end
-
-        it "should show the expense categories for current user and store" do
-          categories = [:category, :category]
-          ExpenseCategory.stub(:for_user_and_store).and_return(categories)
-          get :show, :id => @store.id
-          assigns[:expense_categories].should == categories
-        end
-      end
-    end
+  
+  before :all do
+    User.delete_all
+    Store.delete_all
   end
 
-  context "with stores Chipotle, Baja Fresh, Target and Best Buy existing" do
-    let!(:baja) { Factory(:baja_fresh) }
-    let!(:best_buy) { Factory(:best_buy) }
-    let!(:chipotle) { Factory(:chipotle) }
-    let!(:target) { Factory(:target) }
+  let!(:toad) { User.create(email: "toad@example.com", password: "security123") }
+  let!(:shaw) { User.create(email: "shaw@example.com", password: "blahdeblah3") }
+  let!(:hardware) { Store.create(name: "Family Hardware") }
+  let!(:grocery) { Store.create(name: "Grocery Store") }
+  let!(:gift_shop) { Store.create(name: "Gift Shop") }
 
+  describe :edit do
+
+    it "should require login" do
+      get :edit, id: hardware.to_param
+      response.should redirect_to new_user_session_path
+    end
+
+    it "should load right store" do
+      sign_in toad
+      get :edit, id: hardware.to_param
+      assigns(:store).name.should == hardware.name
+    end
+  
+  end
+  
+  describe :update do
+  
+    it "should not allow a user to change the name" do
+      sign_in toad
+      put :update, id: hardware.to_param, store: { name: "Big Box Hardware" }
+      assigns(:store).name.should == hardware.name
+    end
+  
+    it "should allow a user to assign an expense category" do
+      expected_category = "airfare"
+      sign_in toad
+      put :update, id: hardware.to_param, store: { name: hardware.name, expense_categories: expected_category }
+      
+      actual_category = assigns(:store).expense_categories.first.name
+      
+      actual_category.should == expected_category
+    end
+    
+    it "should allow different expense categories per store for each user" do
+      shaws_category = "business meals"
+      
+      sign_in toad
+      put :update, id: hardware.to_param, store: { name: hardware.name, expense_categories: "airfare" }
+      sign_out toad
+      
+      sign_in shaw
+      put :update, id: hardware.to_param, store: { name: hardware.name, expense_categories: shaws_category }
+      get :show, id: hardware.to_param
+      
+      assigns(:expense_categories).length.should == 1
+      assigns(:expense_categories).first.name.should == shaws_category
+    end
+    
+  end
+
+  describe :show do
+  
+    it "should require login" do
+      get :show, id: hardware.to_param
+      response.should redirect_to new_user_session_path
+    end
+  
+    it "should load the right store" do
+      sign_in toad
+      get :show, id: hardware.to_param
+      assigns(:store).name.should == hardware.name
+    end
+    
+    it "should receive the expense categories for current user and store" do
+      sign_in toad
+      ExpenseCategory.should_receive(:for_user_and_store).with(toad.id, hardware.to_param)
+      get :show, id: hardware.to_param
+    end
+    
+    it "should show the expense categories for current user and store" do
+      categories = ["airfare", "business meals"]
+      sign_in toad
+      ExpenseCategory.stub(:for_user_and_store).and_return(categories)
+      get :show, id: hardware.to_param
+      should assign(:expense_categories).with(categories)
+    end
+    
+  end
+
+  describe :search do
+  
     it "should assign all stores when no search term provided" do
       get :search
-
-      should assign(:stores).with([baja, best_buy, chipotle, target])
+      should assign(:stores).with([hardware, grocery, gift_shop])
     end
-
+    
     it "should return only stores starting with the search term" do
-      get :search, :term => 'b'
-      
-      should assign(:stores).with([baja, best_buy])
+      get :search, term: "g"
+      should assign(:stores).with([grocery, gift_shop])
     end
+  
   end
+
 end
